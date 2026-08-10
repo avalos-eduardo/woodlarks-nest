f3ec635d8c0a4f3e24481d94e2cf20148c93fad8
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2026-08-09 20:18:11 -0500

    implemented footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,25 @@
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { MdMailOutline } from "react-icons/md";
+
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className="flex flex-col bg-[#544635] text-[#EDE8D8] items-center py-10 h-auto gap-5">
+      <h1 className="text-3xl md:text-5xl font-playfair text-center">
+        THE WOODLARK'S NEST
+      </h1>
+      <div className="flex flex-col text-center text-xl font-stretch-110% font-extralight underline-offset-2">
+        <a href="mailto:jesus@thewoodlarksnest.com" className="underline">
+          jesus@thewoodlarksnest.com
+        </a>
+        <a href="tel:+9013000949" className="underline">
+          (901) 300-0949
+        </a>
+      </div>
+      <div className="flex gap-3 text-3xl">
+        <FaFacebookF />
+        <FaInstagram />
+        <MdMailOutline />
+      </div>
+    </footer>
+  );
 }
