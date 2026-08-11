b9c8d81b5a2c37b24f498c3fcf37f4e5cc702e73
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2026-08-10 22:31:23 -0500

    added hero section, fixed navbar and footer fonts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { MdMailOutline } from "react-icons/md";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col bg-[#544635] text-[#EDE8D8] items-center py-10 h-auto gap-5">
-      <h1 className="text-3xl md:text-5xl font-playfair text-center">
+    <footer className="flex flex-col bg-[#544635] text-[#EDE8D8] items-center py-10 h-auto gap-6">
+      <h1 className="text-3xl md:text-5xl font-playfair-display text-center">
         THE WOODLARK'S NEST
       </h1>
-      <div className="flex flex-col text-center text-md md:text-xl font-stretch-110% font-extralight underline-offset-2">
+      <div className="flex flex-col text-center text-md md:text-xl font-lato font-light underline-offset-2">
         <a href="mailto:jesus@thewoodlarksnest.com" className="underline">
           jesus@thewoodlarksnest.com
         </a>
@@ -16,9 +16,15 @@ export default function Footer() {
         </a>
       </div>
       <div className="flex gap-3 text-xl md:text-3xl">
-        <FaFacebookF />
-        <FaInstagram />
-        <MdMailOutline />
+        <a href="https://www.facebook.com/thewoodlarksnest/">
+          <FaFacebookF />
+        </a>
+        <a href="https://www.instagram.com/thewoodlarksnest">
+          <FaInstagram />
+        </a>
+        <a href="mailto:jesus@thewoodlarksnest.com">
+          <MdMailOutline />
+        </a>
       </div>
     </footer>
   );
